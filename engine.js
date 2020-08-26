34ace17a583fbab962fa566a763015eca4799c0f
mailwi <mail.wi@yandex.ru>
2020-08-26 12:06:01 +0300

    Added collision classes for point and rect; added drawing collision; added mouse sprite; added collision functions: whenThisSpriteClicked, touching;

--- engine.js
+++ engine.js
@@ -84,6 +84,16 @@ function gameLoop () {
 
   fps = Math.round(1 / delay)
 
+  for (let i = 0; i < collisionShapes.length; i++) {
+    collisionShapes[i].reset()
+  }
+
+  for (let i = 0; i < collisionShapes.length; i++) {
+    for (let j = i + 1; j < collisionShapes.length; j++) {
+      collisionShapes[i].eval(collisionShapes[j])
+    }
+  }
+
   context.clearRect(0, 0, canvas.width, canvas.height)
 
   if ('forever' in events) {
@@ -93,8 +103,99 @@ function gameLoop () {
     }
   }
 
+  for (let i = 0; i < collisionShapes.length; i++) {
+    fill('rgba(255, 0, 0, 0.25)')
+
+    const collisionShape = collisionShapes[i]
+    if (collisionShape instanceof CollisionRect) {
+      rect(collisionShape.sprite.x + collisionShape.x, collisionShape.sprite.y + collisionShape.y, collisionShape.w, collisionShape.h)
+    } else if (collisionShape instanceof CollisionPoint) {
+      rect(collisionShape.sprite.x + collisionShape.x - 2, collisionShape.sprite.y + collisionShape.y - 2, 4, 4)
+    }
+  }
+
   window.requestAnimationFrame(gameLoop)
 }
+
+/* Collision classes */
+
+class CollisionShape {
+  constructor () {
+    this.collidingShapes = {}
+  }
+
+  reset () {
+    this.collidingShapes = {}
+  }
+
+  addCollidingShape (name, shape) {
+    if (name in this.collidingShapes) {
+      this.collidingShapes[name].push(shape)
+    } else {
+      this.collidingShapes[name] = [shape]
+    }
+  }
+
+  colliding () {
+    if (Object.keys(this.collidingShapes).length > 0) {
+      return true
+    }
+    return false
+  }
+
+  pointRect (px, py, rx, ry, rw, rh) {
+    if (px >= rx && px <= rx + rw && py >= ry && py <= ry + rh) {
+      return true
+    }
+    return false
+  }
+}
+
+class CollisionPoint extends CollisionShape {
+  constructor (sprite, x, y) {
+    super()
+    this.sprite = sprite
+    this.x = x
+    this.y = y
+  }
+
+  eval (collisionShape) {
+    if (collisionShape instanceof CollisionRect) {
+      if (this.pointRect(this.sprite.x + this.x, this.sprite.y + this.y,
+        collisionShape.sprite.x + collisionShape.x,
+        collisionShape.sprite.y + collisionShape.y,
+        collisionShape.w, collisionShape.h)) {
+        this.addCollidingShape(collisionShape.sprite.name, collisionShape)
+        collisionShape.addCollidingShape(this.sprite.name, this)
+      }
+    }
+  }
+}
+
+class CollisionRect extends CollisionShape {
+  constructor (sprite, x, y, w, h) {
+    super()
+    this.sprite = sprite
+    this.x = x
+    this.y = y
+    this.w = w
+    this.h = h
+  }
+
+  eval (collisionShape) {
+    if (collisionShape instanceof CollisionPoint) {
+      if (this.pointRect(collisionShape.sprite.x + collisionShape.x, collisionShape.sprite.y + collisionShape.y,
+        this.sprite.x + this.x,
+        this.sprite.y + this.y,
+        this.w, this.h)) {
+        this.addCollidingShape(collisionShape.sprite.name, collisionShape)
+        collisionShape.addCollidingShape(this.sprite.name, this)
+      }
+    }
+  }
+}
+
+const collisionShapes = []
 
 /* Sprite system */
 
@@ -105,11 +206,35 @@ class Sprite {
     this.x = 0
     this.y = 0
     this.clones = []
+    this.collisionShape = null
+    this.whenThisSpriteClickedEvent = null
   }
 
   goto (x, y) {
     this.x = x
     this.y = y
+  }
+
+  touching (name) {
+    // console.log(this.collisionShape && this.collisionShape.colliding())
+    if (this.collisionShape && this.collisionShape.colliding() && name in this.collisionShape.collidingShapes) {
+      return true
+    }
+    return false
+  }
+
+  whenThisSpriteClicked (code) {
+    this.whenThisSpriteClickedEvent = code
+  }
+
+  collisionPoint (x, y) {
+    this.collisionShape = new CollisionPoint(this, x, y)
+    collisionShapes.push(this.collisionShape)
+  }
+
+  collisionRect (x, y, w, h) {
+    this.collisionShape = new CollisionRect(this, x, y, w, h)
+    collisionShapes.push(this.collisionShape)
   }
 
   setup () {
@@ -123,6 +248,31 @@ function createSprite (name, code) {
   const sprite = new Sprite(name, code)
   sprites[name] = sprite
 }
+
+createSprite('mouse-pointer', function () {
+  this.collisionPoint(0, 0)
+
+  whenGameStart(() => {
+    canvas.addEventListener('mouseup', _ => {
+      if (this.collisionShape.colliding()) {
+        const collidingShapes = this.collisionShape.collidingShapes
+        for (const name in collidingShapes) {
+          const shapes = collidingShapes[name]
+          for (let i = 0; i < shapes.length; i++) {
+            const sprite = shapes[i].sprite
+            if (sprite.whenThisSpriteClickedEvent) {
+              sprite.whenThisSpriteClickedEvent()
+            }
+          }
+        }
+      }
+    })
+
+    forever(() => {
+      this.goto(mouseX, mouseY)
+    })
+  })
+})
 
 /* event functions */
 
--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 function setup () {
   createSprite('Sprite1', function () {
+    this.collisionRect(-25, -25, 50, 50)
+
+    let move = true
+
     whenGameStart(() => {
       this.goto(100, 100)
 
       forever(() => {
-        this.x += delay * 50
+        if (move) this.x += delay * 50
         fill('gray')
         if (mouseDown) {
           fill('red')
         }
+
+        if (this.touching('mouse-pointer')) {
+          console.log('mouse around Sprite1')
+        }
+
         rect(this.x - 25, this.y - 25, 50, 50)
+      })
+
+      this.whenThisSpriteClicked(() => {
+        move = !move
       })
     })
   })
